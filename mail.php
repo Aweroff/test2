4d7028e45187239d1369f04b756b28c1fc9b71b8
Aweroff <131003153+Aweroff@users.noreply.github.com>
2023-07-04 13:26:24 +0300

    mail.php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('aweroff@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('yewiv32097@extemer.com');     // Кому будет уходить письмо
+$mail->addAddress('aweroff@gmail.com');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
